4c70138caa36bcbf2d18b4ee867c1febc57c0fec
SophyRJ <sofiarojasc5@gmail.com>
2025-10-18 21:33:01 +0200

    Ejercicios tema 1 C#

--- C/Ejercicios/EjerciciosCasa1/EjerciciosCasa1/Program.cs
+++ C/Ejercicios/EjerciciosCasa1/EjerciciosCasa1/Program.cs
@@ -9,6 +9,18 @@ namespace EjerciciosCasa1
 {
     internal class Program
     {
+        struct Cancion
+        {
+            public string artista;
+            public string titulo;
+            public int duracion;
+            public double tamañoFichero;
+
+        }
+        struct fichero{
+            public string nombre;
+            public int tamaño;
+        }
         static void Main(string[] args)
         {
             //Primer ejercicio
@@ -188,10 +200,212 @@ namespace EjerciciosCasa1
             //	(1.20) Un programa que prepare espacio para un máximo de 5 nombres.Deberá mostrar al
             //usuario un menú que le permita realizar las siguientes operaciones:
 
-            int[] datos = { 10, 15, 12, 0, 0 };
+            //string[] datos = { "Maria","Sofia",null, null, null };
 
-            int capacidadArray = datos.Length;
-            Console.WriteLine(capacidadArray);
+            //int capacidadArray = datos.Length;
+            //int cantidad = 2;
+            //int opcion = 0;
+            //Console.WriteLine(capacidadArray);
+
+            //do
+            //{
+            //    Console.WriteLine("Elije una opcion: \n1.Añadir un dato" +
+            //        "\n2.Insertar un dato en alguna posicion indicada." +
+            //        "\n3.Boorar un dato" +
+            //        "\n4.Mostrar datos del Array." +
+            //        "\n5.Salir del programa");
+            //    opcion = Convert.ToInt32(Console.ReadLine());
+            //    switch (opcion) 
+            //    {
+            //        case 1:
+            //            Console.Write("Nombre a añadir");
+            //            string nombre = Console.ReadLine();
+            //            datos[cantidad] = nombre;
+            //            cantidad++;
+            //            break;
+            //        case 2:
+            //            if (cantidad == capacidadArray)
+            //            {
+            //                Console.WriteLine("El array esta lleno");
+            //            }
+            //            Console.Write($"El array tiene {cantidad} posiciones.Indique la posicion a la que quiere agregar");
+            //            int pos= Convert.ToInt32(Console.ReadLine());
+            //            Console.WriteLine("Indique el nombre que desea insertar: ");
+            //            string nombre1=Console.ReadLine();
+            //            for(int i=cantidad; i >= pos; i--)
+            //            {
+            //                datos[i] = datos[i - 1];
+            //            }
+            //            datos[pos] = nombre1;
+            //            cantidad++;
+            //            break;
+            //        case 3:
+            //            if (cantidad == 0)
+            //            {
+            //                Console.WriteLine("Nohay datos en el array");
+            //            }
+            //            Console.Write("Indique la posicion que desea borrar: ");
+            //            int posicionBorrar = Convert.ToInt32(Console.ReadLine());
+            //            for(int i = posicionBorrar; i <= cantidad; i++)
+            //            {
+            //                datos[i] = datos[i + 1];
+            //            }
+            //            cantidad--;
+            //            break;
+            //        case 4:
+            //            for(int i = 0; i < cantidad; i++)
+            //            {
+            //                Console.WriteLine("Item{0}: {1}",(i+1),datos[i]);
+            //            }
+            //            break;
+            //        default:
+            //            Console.WriteLine("Saliendo...");
+            //            break;
+            //    }
+
+            //} while (opcion!=5);
+
+            Console.WriteLine("1.21 Ejercicio");
+            //Cancion cancion;
+            //Console.Write("Artista: ");
+            //cancion.artista=Console.ReadLine();
+            //Console.Write("Titulo: ");
+            //cancion.titulo=Console.ReadLine();
+            //Console.Write("Duracion (segundos): ");
+            //cancion.duracion =Convert.ToInt32( Console.ReadLine());
+            //Console.Write("Tamaño (kb): ");
+            //cancion.tamañoFichero =Convert.ToInt32( Console.ReadLine());
+
+            //Console.WriteLine($"Artista: {cancion.artista}, Titulo: {cancion.titulo}, Duracion: {cancion.duracion} sg, Tamaño {cancion.tamañoFichero} Kb");
+
+            Console.WriteLine("1.22 Ejercicio");
+            /*
+             •	(1.22) Un programa que pida al usuario varios números separados por espacios y muestre su suma. */
+
+            //Console.Write("Introduce varios numeros separados por un espacio");
+            //string cadenaNumeros= Console.ReadLine();
+            //int[] numeros= Array.ConvertAll(cadenaNumeros.Split(' '),int.Parse);
+
+
+            //int suma = 0;
+            //foreach(int item in numeros)
+            //{
+            //    suma += item;
+            //}
+            //Console.WriteLine($"La suma es {suma}");
+            //int suma = 0;
+
+            //Console.Write("introduzca varios números separados por espacios");
+            //string Cadena = Console.ReadLine();
+
+            //string[] valores = Cadena.Split(' ');
+            //for (int i = 0; i < valores.Length; i++)
+            //{
+            //    int valor = Convert.ToInt32(valores[i]);
+            //    suma += valor;
+            //}
+            //Console.WriteLine("la suma vale {0}", suma);
+            Console.WriteLine("1.23 Ejercicio");
+            //Console.Write("Escribe una palabra");
+            //string cadena= Console.ReadLine();
+            //StringBuilder nuevaCadena = new StringBuilder(cadena);
+            //for(int i = 0; i < cadena.Length; i++)
+            //{               
+            //    if (i % 2 != 0)
+            //    {
+            //        nuevaCadena[i] = char.ToUpper(cadena[i]);
+            //    }                
+            //}
+            //cadena = nuevaCadena.ToString();
+            //Console.WriteLine(cadena);
+
+            Console.WriteLine("1.24 Ejercicio");
+            /*Un programa que pida tu nombre, tu día de nacimiento y tu mes de nacimiento y lo junte todo en una cadena,
+             * separando el nombre de la fecha 
+             * por una coma y el día del mes por una barra inclinada, así: "Juan, nacido el 31/12". */
+            //Console.Write("Escribe tu nombre ");
+            //string nombre= Console.ReadLine();
+            //Console.Write("Dia nacimiento ");
+            //string dia= Console.ReadLine();
+            //Console.Write("Mes: ");
+            //string mes= Console.ReadLine();
+
+            //StringBuilder datosCompletos= new StringBuilder();
+            //datosCompletos.Append($"{nombre}, nacido el ");
+            //datosCompletos.Append($"{dia}/");
+            //datosCompletos.Append(mes);
+            //Console.WriteLine(datosCompletos.ToString());
+
+            Console.WriteLine("1.25 Ejercicio");
+            fichero[] fichas=new fichero[1000];
+            int cantidad = 0;
+            int opcion=0;
+            string nombre;
+            int tamaño;
+            int numeroFichero;
+            do 
+            {
+                Console.Write("Elija una opcion:\n1.Añadir datos de un nuevo fichero." +
+                    "\n2.Mostrar los nombres de todos lo ficheros " +
+                    "\n3.Mostrar Ficheros que sean de mas e cierto tamaño" +
+                    "\n4.Ver datos de cierto fichero" +
+                    "\n5.Salir de la aplicacion.");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion) 
+                {
+                    case 1:
+                        if (cantidad == 999)
+                        {
+                            Console.WriteLine("No hay espacio");
+                        }
+                        Console.Write("Nombre: ");
+                        nombre=Console.ReadLine();
+                        Console.Write("Tamaño(kb): ");
+                        tamaño=Convert.ToInt32(Console.ReadLine());
+                        fichas[cantidad].nombre = nombre;
+                        fichas[cantidad].tamaño = tamaño;
+                        cantidad++;
+                        break;
+                    case 2:
+                        for(int i=0; i < cantidad; i++)
+                        {
+                            Console.WriteLine($"Ficha {i+1}: {fichas[i].nombre}");
+                        }
+                        break;
+                    case 3:
+                        Console.Write("A partir de que tamaño quieres buscar: ");
+                        tamaño = Convert.ToInt32(Console.ReadLine());
+                        for(int i = 0; i < cantidad; i++)
+                        {
+                            if (fichas[i].tamaño >= tamaño)
+                            {
+                                Console.WriteLine($"Ficha {i + 1}: {fichas[i].nombre}: Tamaño: {fichas[i].tamaño}");
+                            }
+                        }
+                        break;
+                    case 4:
+                        Console.Write("Indica que numero de fichero quieres ver: ");
+                        numeroFichero = Convert.ToInt32(Console.ReadLine());
+                        if (numeroFichero > cantidad)
+                        {
+                            Console.WriteLine("No existe ese fichero");
+                        }
+                        for(int i = 0; i < cantidad; i++)
+                        {
+                            if (numeroFichero - 1 == i)
+                            {
+                                Console.WriteLine($"Fichero {i + 1}, Nombre: {fichas[i].nombre},Tamaño: {fichas[i].tamaño}");
+                            }
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("Saliendo..");
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        break;
+                }
+            } while (opcion!=5);
             Console.ReadKey();
         }
     }
