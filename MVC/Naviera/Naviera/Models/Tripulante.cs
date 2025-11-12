83d48e8ec86811bf6975e9c26988ef557ca0dbef
SophiRJ <sofiarojasc5@gmail.com>
2025-11-12 20:50:40 +0100

    redirecciones

---
+++ MVC/Naviera/Naviera/Models/Tripulante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Naviera.Models
+{
+    public class Tripulante
+    {
+        public int TripulanteId { get; set; }
+        public string NombreTripulante { get; set; }
+        public string Cargo { get; set; }
+        public int BarcoId { get; set; }
+        public DateTime FechaTituloNavegacion { get; set; }
+        public virtual Barco Barco { get; set; }
+    }
+}
